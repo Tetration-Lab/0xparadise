fb5a1bd7653ff6cfcec667ca85d23566b73e4112
b5710546232 <safe.suk@gmail.com>
2023-05-23 19:57:37 +0700

    wip

--- src/constant/menuItem.ts
+++ src/constant/menuItem.ts
@@ -8,16 +8,13 @@ export const menuItems = [
     url: '/account',
   },
   {
-    text: 'Tutorial',
-    url: '/',
+    text: 'No Code',
+    url: 'https://tetration-lab-islander-builder-app-602shh.streamlit.app/',
+    target: '_blank',
   },
   {
     text: 'Twitter',
     url: 'https://twitter.com/Tetration_Lab',
     target: '_blank',
   },
-  // {
-  //   text: 'Game',
-  //   url: '/game',
-  // },
 ]

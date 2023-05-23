5f4540ce3be626e370e1e22ffad4630379459591
b5710546232 <safe.suk@gmail.com>
2023-05-23 21:40:23 +0700

    wip

--- src/view/create-survival/index.tsx
+++ src/view/create-survival/index.tsx
@@ -24,8 +24,8 @@ interface Item {
 
 export const CreateSurvivalPage: NextPage = () => {
   const [imageUrl, setImageUrl] = useState('')
-  const { mutateAsync: createBot } = api.bot.create.useMutation()
-  const { mutateAsync: createSim } = api.simulate.simulate.useMutation()
+  const { mutateAsync: createBot, isLoading: createBotLoading } = api.bot.create.useMutation()
+  const { mutateAsync: createSim, isLoading: simLoading } = api.simulate.simulate.useMutation()
   const { mutateAsync: check } = api.simulate.check.useMutation()
   const botNameRef = useRef<HTMLInputElement>(null)
   const yourCodeRef = useRef<HTMLTextAreaElement>(null)
@@ -94,97 +94,114 @@ export const CreateSurvivalPage: NextPage = () => {
               </Link>
             </div>
             <div className="">Create new survival</div>
+            <div>
+              <Link
+                className="cursor-pointer rounded-md bg-[#B1A6A0] p-2 text-black"
+                href="https://tetration-lab-islander-builder-app-602shh.streamlit.app/"
+                target="_blank"
+              >
+                Create new surival with no code tool
+              </Link>
+            </div>
           </div>
           {/* content */}
           <div className="bg-[#DBD7C6] p-4 pb-16">
             <div className="mt-4 space-y-4 rounded-lg bg-[#B1A6A0] p-4">
-              {/*  */}
-              <div className="space-y-2">
-                <div>Name Your Character</div>
-                <input ref={botNameRef} className="w-full max-w-xl border bg-[#FEF9EB] p-2" />
-              </div>
-              {/* uploader */}
-              <div className="">
-                <div className="mb-4">
-                  <div className="text-lg">Upload your bot Avatar</div>
+              {createBotLoading || simLoading ? (
+                <div className="flex h-auto w-full justify-center">
+                  <Image src="/image/Loading.gif" alt="loading" width={590} height={154} />
                 </div>
-                <div className="h-36 w-36 border-white">
-                  <div className="relative h-full w-full bg-[#FEF9EB]">
-                    <button className="absolute h-full w-full">
-                      <div className="flex flex-col justify-start bg-blue-500"></div>
-                      {/* // eslint-disable-next-line @next/next/no-img-element */}
-                    </button>
-                    {imageUrl ? (
-                      <Image
-                        className='w-full" absolute h-full object-cover'
-                        src={imageUrl}
-                        alt="Picture of the author"
-                        width={320}
-                        height={320}
-                      />
-                    ) : (
-                      <div className="flex h-full w-full items-center justify-center">
-                        <FaImage className="h-8 w-8" />
+              ) : (
+                <>
+                  {/*  */}
+                  <div className="space-y-2">
+                    <div>Name Your Character</div>
+                    <input ref={botNameRef} className="w-full max-w-xl border bg-[#FEF9EB] p-2" />
+                  </div>
+                  {/* uploader */}
+                  <div className="">
+                    <div className="mb-4">
+                      <div className="text-lg">Upload your bot Avatar</div>
+                    </div>
+                    <div className="h-36 w-36 border-white">
+                      <div className="relative h-full w-full bg-[#FEF9EB]">
+                        <button className="absolute h-full w-full">
+                          <div className="flex flex-col justify-start bg-blue-500"></div>
+                          {/* // eslint-disable-next-line @next/next/no-img-element */}
+                        </button>
+                        {imageUrl ? (
+                          <Image
+                            className='w-full" absolute h-full object-cover'
+                            src={imageUrl}
+                            alt="Picture of the author"
+                            width={320}
+                            height={320}
+                          />
+                        ) : (
+                          <div className="flex h-full w-full items-center justify-center">
+                            <FaImage className="h-8 w-8" />
+                          </div>
+                        )}
+                        <div className="absolute inset-0 z-10 h-full w-full bg-red-500 opacity-0">
+                          <UploadButton<OurFileRouter>
+                            endpoint="imageUploader"
+                            onClientUploadComplete={(res) => {
+                              // Do something with the response
+                              if (res && res?.length > 0) {
+                                console.log('Files: ', res[0]?.fileUrl)
+                                const imgUrl = res[0]?.fileUrl as unknown as string
+                                setImageUrl(imgUrl)
+                              }
+                              alert('Upload Completed')
+                            }}
+                            onUploadError={(error: Error) => {
+                              // Do something with the error.
+                              alert(`ERROR! ${error.message}`)
+                            }}
+                          />
+                        </div>
                       </div>
-                    )}
-                    <div className="absolute inset-0 z-10 h-full w-full bg-red-500 opacity-0">
-                      <UploadButton<OurFileRouter>
-                        endpoint="imageUploader"
-                        onClientUploadComplete={(res) => {
-                          // Do something with the response
-                          if (res && res?.length > 0) {
-                            console.log('Files: ', res[0]?.fileUrl)
-                            const imgUrl = res[0]?.fileUrl as unknown as string
-                            setImageUrl(imgUrl)
-                          }
-                          alert('Upload Completed')
-                        }}
-                        onUploadError={(error: Error) => {
-                          // Do something with the error.
-                          alert(`ERROR! ${error.message}`)
-                        }}
-                      />
                     </div>
                   </div>
-                </div>
-              </div>
 
-              <div>
-                <div className="mb-4">Example Template</div>
-                <textarea className="w-full max-w-xl bg-[#FEF9EB]" name="" id="" cols={30} rows={10}></textarea>
-              </div>
+                  <div>
+                    <div className="mb-4">Example Template</div>
+                    <textarea className="w-full max-w-xl bg-[#FEF9EB]" name="" id="" cols={30} rows={10}></textarea>
+                  </div>
 
-              <div>
-                <div className="mb-4">Deploy your code</div>
-                <textarea
-                  ref={yourCodeRef}
-                  className="w-full max-w-xl bg-[#FEF9EB]"
-                  name=""
-                  id=""
-                  cols={30}
-                  rows={10}
-                ></textarea>
-              </div>
+                  <div>
+                    <div className="mb-4">Deploy your code</div>
+                    <textarea
+                      ref={yourCodeRef}
+                      className="w-full max-w-xl bg-[#FEF9EB]"
+                      name=""
+                      id=""
+                      cols={30}
+                      rows={10}
+                    ></textarea>
+                  </div>
 
-              <div>
-                Warning!: By creating new survivor you are deploying a new contract. Any changes to the contract cannot
-                be made until redeployed again blah blah blah
-              </div>
+                  <div>
+                    Warning!: By creating new survivor you are deploying a new contract. Any changes to the contract
+                    cannot be made until redeployed again blah blah blah
+                  </div>
 
-              <div className="space-x-2">
-                <button
-                  onClick={() => void onSubmit()}
-                  className="rounded-lg border border-black bg-[#92C341] px-6 py-3 text-white"
-                >
-                  Create Now
-                </button>
-                <button
-                  onClick={() => void testSim()}
-                  className="rounded-lg border border-black bg-[#f11616] px-6 py-3 text-white"
-                >
-                  Test Sim
-                </button>
-              </div>
+                  <div className="space-x-2">
+                    <button
+                      onClick={() => void onSubmit()}
+                      className="rounded-lg border border-black bg-[#92C341] px-6 py-3 text-white"
+                    >
+                      Create Now
+                    </button>
+                    <button
+                      onClick={() => void testSim()}
+                      className="rounded-lg border border-black bg-[#f11616] px-6 py-3 text-white"
+                    >
+                      Test Sim
+                    </button>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </MainLayout>
